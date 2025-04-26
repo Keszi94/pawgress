dca30141bf43855a9b367d0c4beec72d411fdb9b
Keszi94 <keszi94@gmail.com>
2025-04-26 23:56:18 +0100

    Add courses views and template

--- home/views.py
+++ home/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 
 def index(request):
-    """A view to render the index page"""
+    """A view to render the home page"""
 
     return render(request, 'home/index.html')
