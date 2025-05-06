204d503ca8cab929d51effcdbeb6137e7e37c586
Keszi94 <keszi94@gmail.com>
2025-05-06 22:42:32 +0100

    Update context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,15 +1,32 @@
+from django.shortcuts import get_object_or_404
+from courses.models import Course
 
 
 def cart_contents(request):
     """ Makes the cart data available across all templates """
 
     cart_items = []
-    total = 0
+    # changed total to grand_total to match base.html
+    grand_total = 0
     course_count = 0
+    # get the cart from current section or an empty one
+    cart = request.session.get('cart', {})
+
+    for item_id in cart:
+        # Gets the actual course object
+        course = get_object_or_404(Course, pk=item_id)
+        grand_total += course.price
+        course_count += 1
+
+        cart_items.append({
+            'item_id': item_id,
+            'course': course,
+            'price': course.price,
+        })
 
     context = {
         'cart_items': cart_items,
-        'total': total,
+        'grand_total': grand_total,
         'course_count': course_count,
     }
 
