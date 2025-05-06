a93c165b2f0c722bb89354d487efb4824a2e248c
Keszi94 <keszi94@gmail.com>
2025-05-06 21:50:43 +0100

    Add 'add to cart' view and url pathing

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # shows the cart page
     path('', views.view_cart, name='view_cart'),
+    # adds course to the cart
+    path('add<item_id>/', views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,24 @@ from django.shortcuts import render
 def view_cart(request):
     """ A viw that renders the cart contents page """
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """
+    Adds a course to the cart
+    Only one of each course can be added
+    """
+    redirect_url = request.POST.get('redirect_url')
+
+    cart = request.session.get('cart', {})
+    # convert item_id to a string
+    item_id = str(item_id)
+
+    # Only add a course if it's not in the cart already
+    if item_id not in cart:
+        cart[item_id] = 1
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+
+    return redirect(redirect_url)
