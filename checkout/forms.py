3fd6bcb66bb09a176e83eb53a61936a38c7922f3
Keszi94 <keszi94@gmail.com>
2025-05-11 01:06:50 +0100

    Create purchase form with custom placeholders

---
+++ checkout/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import Purchase
+
+
+class PurchaseForm(forms.ModelForm):
+    class Meta:
+        model = Purchase
+        fields = ('email',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        placeholders = {
+            'email': 'Email address',
+        }
+
+        # sets the autofocus on the email field
+        set.fields['email'].widgets.attrs['autofocus'] = True
+
+        for field_name, field in self.fields.items():
+            # adds a star to required fields
+            placeholder = placeholders.get(field_name, '')
+            if field.required:
+                placeholder += ' *'
+            field.widget.attrs['placeholder'] = placeholder
+
+            # custom css class
+            field.widget.attrs['class'] = 'stripe-style-input'
+            # hides the label - placeholder enough
+            field.label = False
