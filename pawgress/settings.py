2bc5b19914b5f4b92e2771c528f604606e22db95
Keszi94 <keszi94@gmail.com>
2025-05-19 16:52:11 +0100

    AWS fix: delete STATIC_ROOT from settings

--- pawgress/settings.py
+++ pawgress/settings.py
@@ -221,8 +221,6 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # CKEditor
 CKEDITOR_CONFIGS = {
     'default': {
