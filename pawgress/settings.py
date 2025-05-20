242a66c8a6aea5ec06d79520487401fb716e2255
Keszi94 <keszi94@gmail.com>
2025-05-20 19:44:22 +0100

    Add custom email templates; update DEAFULT_FROM_EMAIL with Pawgress name

--- pawgress/settings.py
+++ pawgress/settings.py
@@ -249,7 +249,7 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+    DEFAULT_FROM_EMAIL = 'Pawgress <{}>'.format(EMAIL_HOST_USER)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
