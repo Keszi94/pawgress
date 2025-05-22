de79231d1ddc937b717fdff60e8b9390fb3536de
Keszi94 <keszi94@gmail.com>
2025-05-22 02:38:11 +0100

    Add labels and remove placeholdrs in checkout forms

--- checkout/forms.py
+++ checkout/forms.py
@@ -19,32 +19,21 @@ class PurchaseForm(forms.ModelForm):
                   'street_address1', 'street_address2',
                   'city', 'postcode', 'country')
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        placeholders = {
+        labels = {
             'email': 'Email address',
-            'full_name': 'Full Name',
+            'full_name': 'Full Name*',
             'company_name': 'Company Name (Optional)',
-            'street_address1': 'Street Address 1',
+            'street_address1': 'Street Address 1*',
             'street_address2': 'Street Address 2',
-            'city': 'City',
+            'city': 'City*',
             'postcode': 'ZIP/Postal code',
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         # sets the autofocus on the email field
         self.fields['email'].widget.attrs['autofocus'] = True
 
         for field_name, field in self.fields.items():
-            # exclude country field
-            if field_name != 'country':
-                placeholder = placeholders.get(field_name, '')
-                if field.required:
-                    # adds a star to required fields
-                    placeholder += ' *'
-                field.widget.attrs['placeholder'] = placeholder
-
-            # custom css class
             field.widget.attrs['class'] = 'stripe-style-input'
-            # hides the label - placeholder enough
-            field.label = False
